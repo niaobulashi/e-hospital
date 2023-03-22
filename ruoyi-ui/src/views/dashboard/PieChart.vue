2919879de06c0c498e20781e577a47cf16a6e268
zhangqi <934629976@qq.com>
2023-03-22 10:35:31 +0800

    饼状图比例调整

--- ruoyi-ui/src/views/dashboard/PieChart.vue
+++ ruoyi-ui/src/views/dashboard/PieChart.vue
@@ -31,17 +31,16 @@ export default {
     }
   },
   mounted() {
-    var chartDom = document.getElementById('pieCharId');
-    var myChart = echarts.init(chartDom);
-    var option;
-    option = {
+    let chartDom = document.getElementById('pieCharId');
+    echarts.init(chartDom).setOption({
       tooltip: {
         trigger: 'item',
-        formatter: '{a} <br/>{b} : {c} ({d}%)'
+        formatter: '{a} <br/>{b} : {d}%'
       },
       legend: {
         left: 'center',
-        bottom: '10'
+        bottom: '10',
+        data: ['4h陪诊', '8h陪诊', '代办问诊', '代办买药', '代办约号', '检查预约', '代取报告', 'VIP陪诊']
       },
       series: [
         {
@@ -51,16 +50,26 @@ export default {
           radius: [15, 95],
           center: ['50%', '38%'],
           animationEasing: 'cubicInOut',
-          animationDuration: 2600
+          animationDuration: 2600,
+          data: [
+            {value: 35.08, name: '4h陪诊'},
+            {value: 20.17, name: '8h陪诊'},
+            {value: 9.83, name: '代办问诊'},
+            {value: 12.60, name: '代办买药'},
+            {value: 6.32, name: '代办约号'},
+            {value: 5.28, name: '检查预约'},
+            {value: 4.07, name: '代取报告'},
+            {value: 6.65, name: 'VIP陪诊'}
+          ]
         }
       ]
-    }
-    request.get("/escort/order/projectTypeInfo").then(res => {
-      option.legend.data = res.data.projectName
-      option.series[0].data = res.data.projectTypeInfo
-      myChart.setOption(option);
-    })
+    });
 
+    // request.get("/escort/order/projectTypeInfo").then(res => {
+    //   option.legend.data = res.data.projectName
+    //   option.series[0].data = res.data.projectTypeInfo
+    //   myChart.setOption(option);
+    // })
   },
   beforeDestroy() {
     if (!this.chart) {
